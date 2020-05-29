730b69ebd43390d6ae9331e70ad7b869a76fc28b
looklsh <looklsh38@gmail.com>
2020-05-29 13:11:34 +0900

    method parameter add

---
+++ Method Parameter/Method Parameter/Method Parameter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Parameter
+{
+    class Program
+    {
+        //ref
+        static double GetData(ref int a, ref double b)
+        {
+            return ++a * ++b;
+        }
+
+        //out
+        static bool GetData(int a, int b, out int c, out int d)
+        {
+            c = a + b;
+            d = a - b;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 1;
+            double y = 1.0;
+            double ret = GetData(ref x, ref y);
+            Console.WriteLine(ret);
+
+            int c, d;
+            bool bret = GetData(10, 20, out c, out d);
+            Console.WriteLine(bret);
+        }
+    }
+}
